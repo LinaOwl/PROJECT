d7b26a9f2b6346eb4dfc408649cee249107b20a9
LinaOwl <sorokinamedtech@gmail.com>
2024-08-30 01:59:42 +0300

    without JSX

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,33 +1,54 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { createElement } from "react";
 
-// декларативный
 export const App = () => {
 	const date = new Date();
-	// императивный
 	const showTime =
 		date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
-	// декларативный
 	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-			</header>
-			<div className="App">
-				<h1 align="center">Текущее время</h1>
-				<h2 align="center"> {showTime}</h2>
-			</div>
-		</div>
+		createElement("div", { className: "App" }),
+		createElement("header", { className: "App-header" }),
+		createElement("img", {
+			src: logo,
+			className: "App-logo",
+			alt: "logo",
+		}),
+		createElement(
+			"p",
+			null,
+			"Edit",
+			createElement("code", null, "src/App.js"),
+			"and save to reload.",
+		),
+		createElement(
+			"a",
+			{
+				className: "App-link",
+				href: "https://reactjs.org",
+				target: "_blank",
+				rel: "noopener noreferrer",
+			},
+			"Learn React",
+		),
+		createElement(
+			"div",
+			{
+				className: "App",
+			},
+			"h1",
+			{
+				align: "center",
+			},
+			"Current time",
+		),
+		createElement(
+			"h2",
+			{
+				align: "center",
+			},
+			" ",
+			showTime,
+		)
 	);
 };
